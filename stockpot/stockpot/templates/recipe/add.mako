8c4965371bd2785f07385ef48236fa88eebe5054
Wayne Witzel III <wwitzel3@gmail.com>
2011-12-29 02:25:12 -0500

    Initial recipe view/template
    Signed-off-by: Wayne Witzel III <wwitzel3@gmail.com>

---
+++ stockpot/stockpot/templates/recipe/add.mako
@@ -0,0 +1,10 @@
+<%inherit file="../base.mako"/>
+
+		<div class="row">
+			<div class="span8">
+				Add recipe page
+			</div>
+			<div class="span8">
+				Add recipe page
+			</div>
+		</div>
--- stockpot/stockpot/views/recipe.py
+++ stockpot/stockpot/views/recipe.py
@@ -6,21 +6,10 @@ from pyramid.httpexceptions import HTTPNotFound
 import bson
 import stockpot.models as M
 
-@view_config(route_name='user.profile', renderer='user/profile.mako',
-        request_method='GET')
-def profile(request):
-    user = M.User.query.get(**request.matchdict)
-    if not user:
-        raise HTTPNotFound
-    return dict(user=user)
+@view_config(route_name='recipe.add', renderer='recipe/add.mako', request_method='GET')
+def recipe_add(request):
+    return dict()
 
-@view_config(route_name='user.profile', request_method='POST')
-def profile_update(request):
-    request.user.username = request.params.get('username')
-    M.DBSession.flush()
-    return HTTPFound(location=route_url('user.profile', request,
-                                        username=request.user.username))
-
-def user_routes(config):
-    config.add_route('user.profile', '/{username}')
+def recipe_routes(config):
+    config.add_route('recipe.add', '/add')
 
